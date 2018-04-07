07aecaeb1718cf7fcf53d1cb80c5bb45edc26c3d
DaphneSartorato <daphne.sartorato@gmail.com>
2018-04-07 18:45:19 -0300

    crud funcionario

--- indental/venv/Include/classe.py
+++ indental/venv/Include/classe.py
@@ -1,9 +1,11 @@
 import MySQLdb
 
+
 class NomeClasse:
+
     variavel = 'Daphne'
-    usuario = 'admin'
-    password = 'admin'
+    usuario = 'root'
+    password = 'pitbul12'
 
     def __init__(self):
         return
@@ -22,10 +24,169 @@ class NomeClasse:
     def salvar(self, telefone, tipo, ddd, idPessoa):
         db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
         cursor = db.cursor()
-        cursor.execute("insert into indentalbd.telefone "+
-                       "values(2,'"+telefone+"',"+tipo+",'"+ddd+"',"+idPessoa+")")
+        cursor.execute("insert into indentalbd.telefone (numero,tipo,ddd,idPessoa) "+
+                       "values('"+str(telefone)+"',"+str(tipo)+",'"+str(ddd)+"',"+str(idPessoa)+")")
         #data = cursor.fetchone()
-        #print(data)
-        #print(data[1])
-        # PYTHON É A MELHO LINGUAGEM DO MUNDO!!!
+        return # retorno;
+
+    def atualiza(self, telefone, tipo, ddd, id):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("update indentalbd.telefone set numero = '"+str(telefone)+"',tipo = '"+str(tipo)+"',"
+                        "ddd = '"+str(ddd)+"'" +
+                        "where id = '" + id + "'")
+        #data = cursor.fetchone()
+        return # retorno;
+
+
+class UsuarioDAO:
+
+    variavel = 'Daphne'
+    usuario = 'root'
+    password = 'pitbul12'
+
+    def __init__(self):
+        return
+
+    def funcao(self):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM indentalbd.usuario")
+        data = cursor.fetchall()
+        return data
+
+    def salvar(self, login, senha):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("insert into indentalbd.usuario (dataCadastro, status, login, senha, nivelAcesso)"+
+                       "values(CURDATE(), 1, '"+login+"', '"+senha+"', 'usuario')")
+        id = cursor.lastrowid
+        #print('id gerado: '+str(id))
+        cursor.close()
+        return str(id)
+
+    def atualiza(self, login, senha, id):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("update indentalbd.usuario set login = '"+login+"', senha = '"+senha+"'" +
+                        "where id = '" + id + "'")
+        return
+
+class PessoaDAO:
+
+    variavel = 'Daphne'
+    usuario = 'root'
+    password = 'pitbul12'
+
+    def __init__(self):
+        return
+
+    def funcao(self):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM indentalbd.pessoa")
+        data = cursor.fetchall()
+        return data
+
+    def salvar(self, nome, sobrenome, rg, cpf, sexo, dtnascimento, email):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("insert into indentalbd.pessoa (dataCadastro, status, nome, sobrenome, dataNascimento, sexo, "+
+                       "rg, cpf, email) values(CURDATE(), 1, '"+nome+"', '"+sobrenome+"', "+
+                       "STR_TO_DATE('"+dtnascimento+"','%d/%m/%Y'), '"+sexo + "', '" +rg + "', '" + cpf + "', "+
+                       "'"+email+"')")
+        id = cursor.lastrowid
+        # print('id gerado: '+str(id))
+        cursor.close()
+        return str(id)
+
+    def atualiza(self, nome, sobrenome, rg, cpf, sexo, dtnascimento, email, id):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("update indentalbd.pessoa set nome = '"+nome+"', sobrenome = '"+sobrenome+"', " +
+                        "dataNascimento = STR_TO_DATE('"+dtnascimento+"','%d/%m/%Y'), sexo = '"+sexo+"', " +
+                        "rg = '" + rg + "', cpf = '" + cpf + "', email = '" + email + "'"
+                        "where id = '" + id + "'")
+        id = cursor.lastrowid
+        # print('id gerado: '+str(id))
+        cursor.close()
+        return str(id)
+
+
+class FuncionarioDAO:
+        variavel = 'Daphne'
+        usuario = 'root'
+        password = 'pitbul12'
+
+        def __init__(self):
+            return
+
+        def funcao(self):
+            db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password, db='indentalbd')
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM indentalbd.funcionario as f " +
+                            "inner join indentalbd.usuario u on (u.id = f.idUsuario)" +
+                            "inner join indentalbd.pessoa p on (p.id = f.idPessoa)" +
+                            "inner join indentalbd.endereco e on (e.idPessoa = f.idPessoa)" +
+                            "inner join indentalbd.telefone t on (t.idPessoa = f.idPessoa)")
+            data = cursor.fetchall()
+            return data
+
+        def busca(self, id):
+            db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password, db='indentalbd')
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM indentalbd.funcionario as f " +
+                            "inner join indentalbd.usuario u on (u.id = f.idUsuario)" +
+                            "inner join indentalbd.pessoa p on (p.id = f.idPessoa)" +
+                            "inner join indentalbd.endereco e on (e.idPessoa = f.idPessoa)" +
+                            "inner join indentalbd.telefone t on (t.idPessoa = f.idPessoa)"
+                            "where f.id = "+id)
+            data = cursor.fetchall()
+            return data
+
+        def salvar(self, cargo, salario, idUsuario, idPessoa):
+            db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password, db='indentalbd')
+            cursor = db.cursor()
+            cursor.execute("insert into indentalbd.funcionario (idPessoa, salario, cargo, idUsuario)" +
+                           "values(" + str(idPessoa) + ", " + salario + ", " + cargo + ", " + str(idUsuario)+")")
+            return
+
+        def atualiza(self, cargo, salario, id):
+            db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password, db='indentalbd')
+            cursor = db.cursor()
+            cursor.execute("update indentalbd.funcionario set salario = " + salario + ", cargo = " + cargo +
+                           " where id = '" + id + "'")
+            return
+
+
+class EnderecoDAO:
+
+    variavel = 'Daphne'
+    usuario = 'root'
+    password = 'pitbul12'
+
+    def __init__(self):
+        return
+
+    def funcao(self):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM indentalbd.endereco")
+        data = cursor.fetchall()
+        return data
+
+    def salvar(self, rua, numero, bairro, cidade, estado, cep, IdPessoa):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("insert into indentalbd.endereco (cep, numero, rua, bairro, cidade, estado, idPessoa)"+
+                       "values('"+cep+"', '"+numero+"', '"+rua+"', '"+bairro+"','"+cidade+"','"+estado+"',"+
+                       str(IdPessoa)+")")
+        return
+
+    def atualiza(self, rua, numero, bairro, cidade, estado, cep, id):
+        db = MySQLdb.connect(host='localhost', user=self.usuario, password=self.password , db='indentalbd')
+        cursor = db.cursor()
+        cursor.execute("update indentalbd.endereco set cep = '"+cep+"', numero = '"+numero+"', rua = '"+rua+"', " +
+                        "bairro = '"+bairro+"',cidade = '"+cidade+"',estado = '"+estado+"'" +
+                        "where id = '"+id+"'")
         return
--- indental/venv/Include/teste.py
+++ indental/venv/Include/teste.py
@@ -1,10 +1,11 @@
-#variavel = 'Daphneeee'
-#print('Hello World ' + variavel)
+
+
 from templates import *
-from classe import NomeClasse
+from classe import *#NomeClasse,FuncionarioDAO
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -12,11 +13,13 @@ def hello_world():
     retorno = cls.funcao()
     return render_template('hello.html', var = retorno)
 
-@app.route("/lista")
+
+@app.route("/listaTelefone")
 def lista():
     cls = NomeClasse()
     retorno = cls.funcao()
     return render_template('lista.html', var = retorno)
+
 
 @app.route("/cadastro", methods=['POST'])
 def cadastro():
@@ -32,5 +35,130 @@ def cadastro():
     cls = NomeClasse()
     retorno = cls.salvar(telefone, tipo, ddd, idPessoa)
     return 'oi'#render_template('hello.html', var = retorno)
+
+
+@app.route("/listaFuncionario")
+def listaFuncionario():
+    cls = FuncionarioDAO()
+    retorno = cls.funcao()
+    return render_template('listaFuncionario.html', var = retorno)
+
+
+@app.route("/cadastroFuncionario", methods=['POST'])
+def cadastroFuncionario():
+
+    nome = request.form['txtNome']
+    sobrenome = request.form['txtSobrenome']
+    rg = request.form['txtRg']
+    cpf = request.form['txtCpf']
+    sexo = request.form['txtSexo']
+    dtnascimento = request.form['txtDataNascimento']
+    email = request.form['txtEmail']
+
+    cargo = request.form['txtCargo']
+    salario = request.form['txtSalario']
+
+    login = request.form['txtLogin']
+    senha = request.form['txtSenha']
+
+    rua = request.form['txtRua']
+    numero = request.form['txtNumero']
+    bairro = request.form['txtBairro']
+    cidade = request.form['txtCidade']
+    estado = request.form['txtEstado']
+    cep = request.form['txtCep']
+
+    telefone = request.form['txtTelefone']
+    tipo = request.form['txtTipo']
+    ddd = request.form['txtDdd']
+    comando = request.form['btnComando']
+
+    print('Botao Comando: ' + comando)
+
+    clsusuario = UsuarioDAO()
+    clspessoa = PessoaDAO()
+    clsfuncionario = FuncionarioDAO()
+    clsendereco = EnderecoDAO()
+    clstelefone = NomeClasse()
+
+    idUsuario = 1
+    idPessoa = 1
+
+    idUsuario = clsusuario.salvar(login, senha)
+    idPessoa = clspessoa.salvar(nome, sobrenome, rg, cpf, sexo, dtnascimento, email)
+    clsfuncionario.salvar(cargo, salario, idUsuario, idPessoa)
+    clsendereco.salvar(rua, numero, bairro, cidade, estado, cep, idPessoa)
+    clstelefone.salvar(telefone, tipo, ddd, idPessoa)
+    return render_template('hello.html')
+
+
+@app.route("/insertFuncionario")
+def hello_world2():
+    return render_template('cadastroFuncionario.html')
+
+
+@app.route("/visualizaFuncionario", methods=['POST'])
+def visualizaFuncionario():
+    clsfuncionario = FuncionarioDAO()
+    id = request.form['txtIdFuncionario']
+    retorno = clsfuncionario.busca(id)
+    return render_template('visualizaFuncionario.html', var = retorno)
+
+
+@app.route("/atualizaFuncionario", methods=['POST'])
+def atualizaFuncionario():
+    idUser = request.form['txtIdUsuario']
+    idPes = request.form['txtIdPessoa']
+    idFuncionario = request.form['txtIdFuncionario']
+    idEndereco = request.form['txtIdEndereco']
+    idTelefone= request.form['txtIdTelefone']
+    print(idUser)
+
+    nome = request.form['txtNome']
+    sobrenome = request.form['txtSobrenome']
+    rg = request.form['txtRg']
+    cpf = request.form['txtCpf']
+    sexo = request.form['txtSexo']
+    dtnascimento = request.form['txtDataNascimento']
+    email = request.form['txtEmail']
+
+    cargo = request.form['txtCargo']
+    salario = request.form['txtSalario']
+
+    login = request.form['txtLogin']
+    senha = request.form['txtSenha']
+
+    rua = request.form['txtRua']
+    numero = request.form['txtNumero']
+    bairro = request.form['txtBairro']
+    cidade = request.form['txtCidade']
+    estado = request.form['txtEstado']
+    cep = request.form['txtCep']
+
+    telefone = request.form['txtTelefone']
+    tipo = request.form['txtTipo']
+    ddd = request.form['txtDdd']
+    comando = request.form['btnComando']
+
+    print('Botao Comando: ' + comando)
+
+    clsusuario = UsuarioDAO()
+    clspessoa = PessoaDAO()
+    clsfuncionario = FuncionarioDAO()
+    clsendereco = EnderecoDAO()
+    clstelefone = NomeClasse()
+
+    idUsuario = 1
+    idPessoa = 1
+
+    clsusuario.atualiza(login, senha, idUser)
+    clspessoa.atualiza(nome, sobrenome, rg, cpf, sexo, dtnascimento, email, idPes)
+    clsfuncionario.atualiza(cargo, salario, idFuncionario)
+    clsendereco.atualiza(rua, numero, bairro, cidade, estado, cep, idEndereco)
+    clstelefone.atualiza(telefone, tipo, ddd, idTelefone)
+
+    retorno = clsfuncionario.funcao()
+    return render_template('listaFuncionario.html', var=retorno)
+
 
 app.run()
